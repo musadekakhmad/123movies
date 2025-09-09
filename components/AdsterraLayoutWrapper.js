7036ae377fdcba8ffd4a1a3c7719de851ba2765a
musadek akhmad <musadekakhmad@gmail.com>
2025-09-09 21:40:37 +0700

    Update AdsterraLayoutWrapper.js

--- components/AdsterraLayoutWrapper.js
+++ components/AdsterraLayoutWrapper.js
@@ -16,8 +16,28 @@ export default function AdsterraLayoutWrapper({ children }) {
 
     window.addEventListener('click', handleClick);
 
+    // Adsterra Social Bar and Popunder scripts
+    const addAdsterraScripts = () => {
+      // Social Bar
+      const socialBarScript = document.createElement('script');
+      socialBarScript.type = 'text/javascript';
+      socialBarScript.src = '//discreetisabella.com/cbe005efaae7ab20e3faa2899671b795.js';
+      document.body.appendChild(socialBarScript);
+
+      // Popunder
+      const popunderScript = document.createElement('script');
+      popunderScript.type = 'text/javascript';
+      popunderScript.src = '//discreetisabella.com/e0f83591c34e956e825dd77e782c86d3.js';
+      document.body.appendChild(popunderScript);
+    };
+
+    addAdsterraScripts();
+
     return () => {
       window.removeEventListener('click', handleClick);
+      // Clean up the scripts when the component unmounts
+      const scripts = document.querySelectorAll('script[src*="discreetisabella.com"]');
+      scripts.forEach(script => script.remove());
     };
   }, []);
 
